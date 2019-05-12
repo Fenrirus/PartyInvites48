3033351c19205031741be5e22de1f4b463e7d6b9
Marian Marian <fenrir55@gmail.com>
2019-05-12 12:32:40 +0200

    Add project files.

---
+++ PartyInvites48/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace PartyInvites48.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
