84ff63a94580f21c0978589afe31b06a63e242d3
Marian Marian <fenrir55@gmail.com>
2019-05-12 16:24:26 +0200

    style

--- PartyInvites48/Controllers/HomeController.cs
+++ PartyInvites48/Controllers/HomeController.cs
@@ -5,13 +5,35 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using PartyInvites48.Models;
 
     public class HomeController : Controller
     {
         // GET: Home
         public ViewResult Index()
         {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczów";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ PartyInvites48/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace PartyInvites48.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać imię i nazwisko")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Proszę podać prawidłowy email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Proszę podać nr telefonu")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Czy weźmiesz udział")]
+        public bool? WillAttend { get; set; }
+    }
+}
